d98d521951d84888f3c1e5967e3da1b0efe5b033
Jameson Athanasiou <jamesonathanasiou@gmail.com>
2022-09-10 08:22:59 -0400

    setup for publish

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-import { getUser } from './apis/user'
+import { getAvatarThumbnail, getAvatar } from './apis/avatar'
+import { getHeadToHeadMatchupsByWeek, getMatchupsByWeek } from './apis/league'
+import { getUserByUsername, getUserById } from './apis/user'
+
+export default {
+  getAvatar,
+  getAvatarThumbnail,
+  getHeadToHeadMatchupsByWeek,
+  getMatchupsByWeek,
+  getUserById,
+  getUserByUsername,
+}
