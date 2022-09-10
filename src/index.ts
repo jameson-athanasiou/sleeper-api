2ce620f875682e4f1e7e20b2030de01573fcaa01
Jameson Athanasiou <jamesonathanasiou@gmail.com>
2022-09-10 11:26:51 -0400

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,4 @@ import { getAvatarThumbnail, getAvatar } from './apis/avatar'
 import { getHeadToHeadMatchupsByWeek, getMatchupsByWeek } from './apis/league'
 import { getUserByUsername, getUserById } from './apis/user'
 
-export default {
-  getAvatar,
-  getAvatarThumbnail,
-  getHeadToHeadMatchupsByWeek,
-  getMatchupsByWeek,
-  getUserById,
-  getUserByUsername,
-}
+export { getAvatar, getAvatarThumbnail, getHeadToHeadMatchupsByWeek, getMatchupsByWeek, getUserById, getUserByUsername }
